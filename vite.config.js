f55a3c5d736e727fda4ca84fa863957986c93603
Matt Winchester <latterarrays@gmail.com>
2024-04-15 15:54:50 -0400

    Flashing buttons, fun orbits

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   css: {
     modules: true,
-  }
+  },
+  server: {
+    watch: {
+      include: 'src/**/*.js',
+    },
+  },
 });
